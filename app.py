86a1e02f406ed75149a81368d1a8bd5a3f55751a
fyukina06 <fyukina06@gmail.com>
2026-04-22 21:47:55 +0900

    simplify app and add top5 lip results

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+st.markdown("""
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXX"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-JQ9KE2MMRZ');
+</script>
+""", unsafe_allow_html=True)
+
 import streamlit as st
 from PIL import Image
 from streamlit_image_coordinates import streamlit_image_coordinates
